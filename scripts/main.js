a23eb8c3f43f72f814f9d81198a860bafde1e2ce
donrushk <emailfrancesparaalvarito@gmail.com>
2018-08-31 23:06:17 +0200

    wat1

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,39 @@ else {
 miBoton.onclick = function() {
     estableceNombreUsuario();
 }
+
+
+
+//WATSON
+
+// Example 1: sets up service wrapper, sends initial message, and 
+// receives response.
+
+var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+// Set up Assistant service wrapper.
+var service = new AssistantV1({
+  username: '3fc683ee-50bb-48ab-89bd-b1b7104c1564', // replace with service username
+  password: '8GzFKXU7QBQ3', // replace with service password
+  version: '2018-02-16'
+});
+
+var workspace_id = 'b368b782-a212-4385-8d15-5aab22543a19'; // replace with workspace ID
+
+// Start conversation with empty message.
+service.message({
+  workspace_id: workspace_id
+  }, processResponse);
+
+// Process the service response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  // Display the output from dialog, if any.
+  if (response.output.text.length != 0) {
+      console.log(response.output.text[0]);
+  }
+}
